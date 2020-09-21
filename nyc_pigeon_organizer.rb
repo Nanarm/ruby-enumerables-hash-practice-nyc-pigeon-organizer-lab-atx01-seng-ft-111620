c2afd6d54417f449ed76160a699908f5c579638a
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-21 03:28:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  data.each do |name, :color, :gender, lives|
+  data.each do |name, :color, :gender, :lives|
     name = :name
      new.Hash pigeon_data
     
